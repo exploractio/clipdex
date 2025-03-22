6267bf1919a5f7d07af1334960ff2ab3b98cec3b
exploractio <175453490+exploractio@users.noreply.github.com>
2025-03-23 00:54:49 +0100

    Update my-videos.blade.php

--- resources/views/livewire/my-videos.blade.php
+++ resources/views/livewire/my-videos.blade.php
@@ -3,9 +3,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
                 <div class="flex justify-between items-center mb-6">
-                    <h2 class="text-2xl font-bold">Mis Vídeos</h2>
+                    <h2 class="text-2xl font-bold">Mis vídeos</h2>
                     <a href="{{ route('videos.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        Nuevo Vídeo
+                        Subir un clip
                     </a>
                 </div>
 
